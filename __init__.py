b6237fa7d95694f8e7ed2e96ca2676eff80519c3
mseewer <marco.seewer@gmail.com>
2020-07-30 10:37:09 +0200

    add first code version

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from datetime import datetime
 
 class SkillRecappTime(MycroftSkill):
     def __init__(self):
@@ -7,11 +7,25 @@ class SkillRecappTime(MycroftSkill):
 
     @intent_file_handler('time.recapp.skill.intent')
     def handle_time_recapp_skill(self, message):
-        time = ''
+        now = datetime.now()
 
+        hours = now.strftime("%H")
+        curr_minutes = round_time(now.strftime("%M"))
+
+        minutes = round_time(curr_minutes)
         self.speak_dialog('time.recapp.skill', data={
-            'time': time
+            'hours': hours,
+            "minutes": minutes
         })
+
+    def round_time(self, curr_minutes):
+        minutes = int(curr_minutes)
+        rest = minutes % 5
+        if (rest > 3): # round up
+            return minutes + 5 - rest
+        else:
+            return minutes - rest
+
 
 
 def create_skill():
