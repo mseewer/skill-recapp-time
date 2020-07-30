9c5a91469c64bbec3a95db1355f6f9a2c9076c00
mseewer <marco.seewer@gmail.com>
2020-07-30 11:48:19 +0200

    add dialog files and support for them in pyton code

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 from datetime import datetime
 import pytz
+import random
 
 class SkillRecappTime(MycroftSkill):
     def __init__(self):
@@ -13,22 +14,21 @@ class SkillRecappTime(MycroftSkill):
 
         self.hours = now.strftime("%H")
         self.minutes = self.round_time(now.strftime("%M"))
-        self.log.info("rounding minutes done")
+        self.log.info("rounding minutes done. Start with output:")
 
         if (minutes == 0):
             self.output_full_hour()
         elif (minutes % 30 == 0):
             self.output_half_hour()
-        elif (minutes % 15 == 0):
-            self.output15()
+        elif (minutes == 15):
+            self.output_15()
+        elif (minutes == 45):
+            self.output_45()
         elif (minutes < 30):
             self.output_ab()
         else:
             self.output_vor()
-        self.speak_dialog('time.start', data={
-            'hours': hours,
-            "minutes": minutes
-        })
+
 
     def round_time(self, curr_minutes):
         minutes = int(curr_minutes)
@@ -38,8 +38,57 @@ class SkillRecappTime(MycroftSkill):
         else:
             return minutes - rest
 
-    def output_full_hour():
-        pass
+    def output_full_hour(self):
+        files = ["time.full.hour"]
+        dialog_file = random.choice(files)
+        self.speak_dialog(dialog_file, data={
+            'hours': self.hours
+        })
+
+    def output_half_hour(self):
+        files = ["time.base", "time.half.hour"]
+        dialog_file = random.choice(files)
+        self.speak_dialog(dialog_file, data={
+            'hours': self.hours
+        })
+
+    def output_15(self):
+        files = ["time.base", "time.15", "time.ab"]
+        dialog_file = random.choice(files)
+        self.speak_dialog(dialog_file, data={
+            'hours': self.hours, 
+            'minutes': self.minutes
+        })
+
+    def output_45(self):
+        files = ["time.base", "time.45", "time.ab"]
+        dialog_file = random.choice(files)
+        self.speak_dialog(dialog_file, data={
+            'hours': self.hours, 
+            'minutes': self.minutes
+        })
+
+    def output_ab(self):
+        files = ["time.base", "time.ab"]
+        dialog_file = random.choice(files)
+        self.speak_dialog(dialog_file, data={
+            'hours': self.hours, 
+            'minutes': self.minutes
+        })
+
+    def output_vor(self):
+        files = ["time.base", "time.vor"]
+        dialog_file = random.choice(files)
+        if (dialog_file == "time.base"):
+            self.speak_dialog(dialog_file, data={
+                'hours': self.hours, 
+                'minutes': self.minutes
+            })
+        else: 
+            self.speak_dialog(dialog_file, data={
+                'hours': self.hours, 
+                'minutes': (60 - self.minutes)
+            })
 
 
 def create_skill():
